38c653d36853e4723538933796da40595be77d6d
ARUN KOCHUKADAVIL <arunkochukadavil10@gmail.com>
2019-03-06 22:54:58 +0530

    Image Receiver Part Added

--- ImageTransferReceiveAPI/Controllers/HomeController.cs
+++ ImageTransferReceiveAPI/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -13,6 +15,33 @@ namespace ImageTransferReceiveAPI.Controllers
 			ViewBag.Title = "Home Page";
 
 			return View();
+		}
+		public ActionResult ImageReceiver()
+		{
+			try
+			{
+				Stream req = Request.InputStream;
+				req.Seek(0, System.IO.SeekOrigin.Begin);
+				string json = new StreamReader(req).ReadToEnd();
+				JObject jsonObj = JObject.Parse(json);
+				var content = (string)jsonObj["base64"];
+				byte[] imageBytes = Convert.FromBase64String(content);
+				MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
+				var name = (string)jsonObj["name"];
+				var path = @"C:\Users\Arun\Desktop\project\Matlab-Download\ProjectHGR\" + name + ".jpg";
+				ms.Write(imageBytes, 0, imageBytes.Length);
+				System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
+				image.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
+				var val = new { status = "Received" };
+				return Json(val, JsonRequestBehavior.AllowGet);
+			}
+			catch (Exception e)
+			{
+				var val = new { status = e.Message };
+				return Json(val, JsonRequestBehavior.AllowGet);
+			}
+			
+			//return View();
 		}
 	}
 }
