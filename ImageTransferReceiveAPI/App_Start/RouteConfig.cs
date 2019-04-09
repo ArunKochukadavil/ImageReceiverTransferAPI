adf48e576173e926d4f614d7dcd4c537bd4db22b
ARUN KOCHUKADAVIL <arunkochukadavil10@gmail.com>
2019-04-10 00:58:32 +0530

    WebAPI basic wrapper completed for image Receiving via json and sending result in json back as response

--- ImageTransferReceiveAPI/App_Start/RouteConfig.cs
+++ ImageTransferReceiveAPI/App_Start/RouteConfig.cs
@@ -18,6 +18,7 @@ namespace ImageTransferReceiveAPI
 				url: "{controller}/{action}/{id}",
 				defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
 			);
+			
 		}
 	}
 }
--- ImageTransferReceiveAPI/Controllers/HomeController.cs
+++ ImageTransferReceiveAPI/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Web;
@@ -23,31 +24,63 @@ namespace ImageTransferReceiveAPI.Controllers
 				Stream req = Request.InputStream;
 				req.Seek(0, System.IO.SeekOrigin.Begin);
 				string json = new StreamReader(req).ReadToEnd();
+
 				JObject jsonObj = JObject.Parse(json);
 				var content = (string)jsonObj["base64"];
 				byte[] imageBytes = Convert.FromBase64String(content);
 				MemoryStream ms = new MemoryStream(imageBytes, 0, imageBytes.Length);
-				var name = (string)jsonObj["name"];
-				var path = @"C:\Users\Arun\Desktop\project\Matlab-Download\ProjectHGR\" + name;
+				var name = (string)jsonObj["name2"];
+				var path = @"F:\Arun\B-TECH\project\ProjectHGR\ProjectHGR\Project\Images\Inputs\sample\" + name;
 				ms.Write(imageBytes, 0, imageBytes.Length);
 				System.Drawing.Image image = System.Drawing.Image.FromStream(ms, true);
 				image.Save(path, System.Drawing.Imaging.ImageFormat.Jpeg);
-				var val = new { status = "Received" };
+				var line = startProcess(path);
+				//var line = name;
+				var val = new { status = line };
 				return Json(val, JsonRequestBehavior.AllowGet);
 			}
 			catch (Exception e)
 			{
 				var val = new { status = e.Message };
 				return Json(val, JsonRequestBehavior.AllowGet);
+
 			}
 			
 			//return View();
 		}
-		public ActionResult GetOutPut(string name)
+
+		public ActionResult GetOutPut(string id)
 		{
-			var path = @"C:\Users\Arun\Desktop\project\Matlab-Download\ProjectHGR\" + name + ".txt";
+			var path = id;
+			//var path = @"C:\Users\Arun\Desktop\project\Matlab-Download\ProjectHGR\" + id + ".txt";
 			var json = new { data = new Data().getImageOutput(path) };
 			return Json(json, JsonRequestBehavior.AllowGet);
+		}
+		public string startProcess(string path)
+		{
+			ProcessStartInfo pStartInfo = new ProcessStartInfo();
+			pStartInfo.FileName = @"python";
+			//pStartInfo.RedirectStandardError=true;
+			pStartInfo.RedirectStandardOutput = true;
+			pStartInfo.UseShellExecute = false;
+			string pythonScriptPath = Server.MapPath("~/App_Data/datamodeltestOnlyForTest.py");
+			string pythonScriptArgForSpecifyingLocationOfModelWithJsonExtension= Server.MapPath("~/App_Data/files/model_basic.json");
+			string pythonScriptArgForSpecifyingLocationOfModelContainingWeights = Server.MapPath("~/App_Data/files/model_basic.h5");
+
+			pStartInfo.Arguments = $"{ pythonScriptPath} {path} {pythonScriptArgForSpecifyingLocationOfModelWithJsonExtension} {pythonScriptArgForSpecifyingLocationOfModelContainingWeights}";
+			Process p = new Process();
+			p.StartInfo = pStartInfo;
+			p.Start();
+			string line = "hello";
+			p.WaitForExit();
+			while (!p.StandardOutput.EndOfStream)
+			{
+				line = p.StandardOutput.ReadLine();
+				// do something with line
+			}
+
+			Console.WriteLine(line);
+			return line;
 		}
 
 	}
